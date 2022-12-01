9ce74a4d21fa90ba0e58bb9a9e26404a7e7d019d
trippymajo <88328353+trippymajo@users.noreply.github.com>
2022-12-01 12:13:20 +0300

    Initial commit
    Bot is alive! Going online on Discord.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace jumpPochinkiBot
+{
+	class Program
+	{
+		private DiscordSocketClient client;
+		private CommandService commands;
+		public static Task Main(string[] args)
+			=> new Program().MainAsync();
+
+		public async Task MainAsync()
+		{
+			client = new DiscordSocketClient();
+			client.Log += Log;
+
+			var token = File.ReadAllText("C:\\Users\\Admin\\source\\repos\\jumpPochinkiBot\\discordToken.txt");
+
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+
+			await Task.Delay(-1);
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		
+	}
+}
